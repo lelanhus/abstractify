7af772e42d22de16d385465ff14a59692044022e
Leland Husband <leland.husband@gmail.com>
2011-02-12 16:19:27 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abstractify::Application.config.secret_token = 'a72c2f17cfbe4fbce8d9c8d419b02a2a16dcdb8e48e448f209f944f434bad61e8c443296d3cbb2442157b3bad110329f04d3d8687ba728c15b1a1682a457352b'
