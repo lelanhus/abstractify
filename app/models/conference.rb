a527b7882734cfd8778924be88473c4110a7987f
Leland Husband <leland.husband@gmail.com>
2011-02-12 18:36:00 -0600

    set default timezone

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,5 +1,7 @@
 class Conference < ActiveRecord::Base
   has_many :submissions
   
+  scope :accepting_submissions, where("conferences.submission_deadline <= ?", Time.now)
+  
   validates :name, :start_date, :end_date, :submission_deadline, :presence => true
 end
