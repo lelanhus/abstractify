8043e310dcac0459a2ceec5af58dd877dbb42776
Leland Husband <leland.husband@gmail.com>
2011-02-15 12:40:04 -0600

    another stab at pdfkit

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,4 +1,4 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-i386').to_s if ::Rails.env.production?
+  config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if ::Rails.env.production?
   config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-mac').to_s if ::Rails.env.development?
 end
